035e5118087637d61c24aa7065547d27aea377fd
jérémy Réchet <jeremyr@hotmail.fr>
2018-02-06 12:07:13 +0100

    finish trackTO

---
+++ src/main/java/poe/jsf/WebConfiguration.java
@@ -0,0 +1,10 @@
+package poe.jsf;
+//@Configuration
+public class WebConfiguration {
+//    @Bean
+//    ServletRegistrationBean h2servletRegistration(){
+//        ServletRegistrationBean registrationBean = new ServletRegistrationBean( new WebServlet());
+//        registrationBean.addUrlMappings("/console/*");
+//        return registrationBean;
+//    }
+}
